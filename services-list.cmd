0a4f861e3cd12faa2ad18784c2847d2576c0e15d
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-12-24 14:35:45 +0000

    MSSQL Server and Postgresql (#2)
    Added MSSQL Server and Postgresql

--- services-list.cmd
+++ services-list.cmd
@@ -1,10 +1,15 @@
 SET DOCKER_DEVSVC_PROJECT=devsvc
 SET DOCKER_DEVSVC_COMPOSE_FILES=^
  -f compose-elasticsearch.yml^
- -f compose-logstash.yml^
  -f compose-kibana.yml^
  -f compose-mongo.yml^
  -f compose-rabbitmq.yml^
- -f compose-redis.yml^
  -f compose-eventstore.yml^
  -f network.yml
+
+
+REM Add to the list above as required
+REM -f compose-logstash.yml^
+REM -f compose-redis.yml^
+REM -f compose-sqlserver.yml^
+REM -f compose-postgres.yml^
--- services-list.sh
+++ services-list.sh
@@ -1,11 +1,16 @@
 DOCKER_DEVSVC_PROJECT=devsvc
 DOCKER_DEVSVC_COMPOSE_FILES="\
  -f compose-elasticsearch.yml\
- -f compose-logstash.yml\
  -f compose-kibana.yml\
  -f compose-mongo.yml\
  -f compose-rabbitmq.yml\
- -f compose-redis.yml\
  -f compose-eventstore.yml\
  -f network.yml\
  "
+
+# Add to the list above as required:
+# -f compose-logstash.yml\
+# -f compose-redis.yml\
+# -f compose-sqlserver.yml\
+# -f compose-postgres.yml\
+ 
