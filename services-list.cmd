ae21d08921ced072a4b02c49ddf422b632b753ed
Ivan Blazhko <ivan.blazhko@gmail.com>
2019-06-08 08:01:37 +0100

    Disabled some services

--- services-list.cmd
+++ services-list.cmd
@@ -3,13 +3,8 @@ SET DOCKER_DEVSVC_COMPOSE_FILES=^
  -f compose-elasticsearch.yml^
  -f compose-eventstore.yml^
  -f compose-kibana.yml^
- -f compose-logstash.yml^
  -f compose-mongo.yml^
- -f compose-postgres.yml^
  -f compose-rabbitmq.yml^
- -f compose-redis.yml^
- -f compose-sqlserver.yml^
- -f compose-vault.yml^
  -f network.yml
 
 
